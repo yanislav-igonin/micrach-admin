381c8769faeb2ee71171a7d13a2921698b26b9b3
Yanislav Igonin <yanislavdabest@gmail.com>
2022-05-02 12:24:04 +0300

    login page done

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,9 +1,10 @@
-import { TextInput, Button } from '@mantine/core';
+import { TextInput, Button, Center, Container } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import Router  from 'next/router';
 import { showNotification } from '@mantine/notifications';
 import { login as loginApi, LoginRequest } from 'lib/api/auth/login';
 import { AxiosError } from 'axios';
+import classes from './login.module.css';
 
 const login = async (data: LoginRequest) => {
   try {
@@ -26,11 +27,13 @@ const Login = () => {
 
   return (
     <main>
-      <form onSubmit={form.onSubmit((values) => login(values))}>
-        <TextInput placeholder='Login' {...form.getInputProps('username')} />
-        <TextInput placeholder='Password' type='password' {...form.getInputProps('password')} />
-        <Button type='submit'>Login</Button>
-      </form>
+      <Center className={classes['login-page-container']}>
+        <form className={classes['login-form-container']} onSubmit={form.onSubmit((values) => login(values))}>
+          <TextInput mb='md' placeholder='Login' {...form.getInputProps('username')} />
+          <TextInput mb='md' placeholder='Password' type='password' {...form.getInputProps('password')} />
+          <Button type='submit'>Login</Button>
+        </form>
+      </Center>
     </main>
   );
 };
